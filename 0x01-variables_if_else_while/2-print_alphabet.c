09b22fc09bd1398a5d4bd6a54c5c6850dc82c8b5
Johnfavour <favourjohn851@gmail.com>
2022-10-14 04:58:04 -0700

    Print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
