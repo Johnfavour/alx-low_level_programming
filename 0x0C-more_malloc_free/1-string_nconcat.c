91df74860f90eb01ae1166a5d67934a0e7bda163
Johnfavour <favourjohn851@gmail.com>
2022-11-10 20:37:10 -0800

    tasks1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		a = 0;
 	else
 	{
-		for (a = 0; s1[a]; a++);
+		for (a = 0; s1[a]; a++)
+			;
 	}
 	if (s2 == NULL)
 		b = 0;
 	else
 	{
-		for (b = 0; s2[b]; b++);
+		for (b = 0; s2[b]; b++)
+			;
 	}
 	if (b > n)
 		b = n;
